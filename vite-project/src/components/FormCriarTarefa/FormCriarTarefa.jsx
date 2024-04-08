7bef4fa76aa8102659c98db6c539064c0f54537d
Cesar Augusto <cesinhalius@hotmail.com>
2024-04-08 14:44:56 -0300

    Adicionando a função de adicionar tarefa e listar elas

--- vite-project/src/components/FormCriarTarefa/FormCriarTarefa.jsx
+++ vite-project/src/components/FormCriarTarefa/FormCriarTarefa.jsx
@@ -1,15 +1,48 @@
-
+import { useState } from "react";
 
 import { Botao, CampoTexto } from "../../components";
 
 import style from "./FormCriarTarefa.module.css";
 
-const FormCriarTarefa = () => {
+const FormCriarTarefa = (props) => {
 
+    const [nomeTarefa, setNomeTarefa] = useState('');
+
+    const { setTarefas } = props;
+
+    const atualizarNome = (event) => {
+        setNomeTarefa(event.currentTarget.value)
+    };
+
+    const adicionarTarefa = (event) => {
+        event.preventDefault();
+
+        if(!nomeTarefa){
+            return;
+        }
+
+        setTarefas(estatoAtual => {
+            const tarefa = {
+                id: estatoAtual.length + 1,
+                nome: nomeTarefa,
+            };
+
+            return [
+                ...estatoAtual,
+                tarefa,
+
+            ];
+        });
+
+        setNomeTarefa('');
+    };
 
     return(
-        <form className={style.FormCriarTarefa} >
-            <CampoTexto/>
+        <form className={style.FormCriarTarefa} onSubmit={adicionarTarefa}>
+            <CampoTexto 
+             value={nomeTarefa} 
+             onChange={atualizarNome}
+            />
             <Botao texto=" + "/>
         </form>
     )
--- vite-project/src/components/ListaTarefa/ListaTarefa.jsx
+++ vite-project/src/components/ListaTarefa/ListaTarefa.jsx
@@ -2,12 +2,8 @@ import style from './ListaTarefa.module.css';
 
 import { ListaTarefaItem } from "./ListaTarefaItem";
 
-const ListaTarefa = () => {
-    const tarefas = [
-        {id: 1, nome:"Item 1"},
-        {id: 2, nome:"Item 2"},
-        {id: 3, nome:"Item 3"},
-    ];
+const ListaTarefa = (props) => {
+    const { tarefas } = props;
     return(
         <ul className={style.ListaTarefa}>
             {tarefas.map(item => <ListaTarefaItem key={item.id} nomeTarefa={item.nome} />)}
